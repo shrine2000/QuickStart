bb5487eccb8801f1dac4158d861fc1bf72f3bff5
Shrine Sabu <shrine02sabu@gmail.com>
2023-05-20 21:39:04 +0530

    added sidebar

--- main.py
+++ main.py
@@ -1,160 +1,103 @@
-import os
-import subprocess
+from PyQt5.QtWidgets import *
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QLineEdit, QPushButton, QComboBox, QTextEdit
+from gui.new_project_window import NewProjectWindow
+from gui.readme_generator_gui import ReadmeGeneratorGUI
+from gui.about_widget import AboutWidget
 
-
-class NewProjectWindow(QMainWindow):
+class Window(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        # Set the window properties
-        self.setWindowTitle("New Project")
-        self.setGeometry(100, 100, 400, 250)
+         
+        self.setWindowTitle('Quick Start')
 
-        # Create the label and text box for the project name
-        self.project_name_label = QLabel("Project Name:", self)
-        self.project_name_label.move(50, 50)
-        self.project_name_textbox = QLineEdit(self)
-        self.project_name_textbox.move(150, 50)
-        self.project_name_textbox.resize(200, 25)
+        self.Width = 800
+        self.height = int(0.618 * self.Width)
+        self.resize(self.Width, self.height)
 
-        # Create the license label and drop-down box
-        self.license_label = QLabel("License:", self)
-        self.license_label.move(50, 100)
-        self.license_dropdown = QComboBox(self)
-        self.license_dropdown.addItem("Apache License 2.0")
-        self.license_dropdown.addItem("BSD 3-Clause License")
-        self.license_dropdown.addItem("MIT License")
-        self.license_dropdown.move(150, 100)
-        self.license_dropdown.resize(200, 25)
+         
+        self.btn_1 = QPushButton('New Project', self)
+        self.btn_2 = QPushButton('Readme', self)
+        self.btn_3 = QPushButton('About Us', self)
+ 
+        self.btn_1.clicked.connect(self.button1)
+        self.btn_2.clicked.connect(self.button2)
+        self.btn_3.clicked.connect(self.button3)
+ 
+        self.tab1 = self.ui1()
+        self.tab2 = self.ui2()
+        self.tab3 = self.ui3()
+ 
+        self.initUI()
 
-        # Create the label and text box for the project directory
-        self.project_directory_label = QLabel("Project Directory:", self)
-        self.project_directory_label.move(50, 150)
-        self.project_directory_textbox = QLineEdit(self)
-        self.project_directory_textbox.move(150, 150)
-        self.project_directory_textbox.resize(150, 25)
-        self.browse_button = QPushButton("Browse", self)
-        self.browse_button.move(310, 150)
-        self.browse_button.clicked.connect(self.browse_directory)
+    def initUI(self):
+        left_layout = QVBoxLayout()
+        left_layout.addWidget(self.btn_1)
+        left_layout.addWidget(self.btn_2)
+        left_layout.addWidget(self.btn_3)
+        left_layout.addStretch(5)
+        left_layout.setSpacing(20)
+        left_widget = QWidget()
+        left_widget.setLayout(left_layout)
 
-        # Create the label and text box for the GitHub repo URL
-        self.github_url_label = QLabel("GitHub Repo URL:", self)
-        self.github_url_label.move(50, 200)
-        self.github_url_textbox = QLineEdit(self)
-        self.github_url_textbox.move(150, 200)
-        self.github_url_textbox.resize(200, 25)
-        
-    
+        self.right_widget = QTabWidget()
+        self.right_widget.tabBar().setObjectName("mainTab")
 
-        # Create the create button
-        self.create_button = QPushButton("Create", self)
-        self.create_button.move(150, 250)
-        self.create_button.clicked.connect(self.create_project)
+        self.right_widget.addTab(self.tab1, '')
+        self.right_widget.addTab(self.tab2, '')
+        self.right_widget.addTab(self.tab3, '')
+ 
+        self.right_widget.setCurrentIndex(0)
+        self.right_widget.setStyleSheet('''QTabBar::tab{width: 0; \
+            height: 0; margin: 0; padding: 0; border: none;}''')
 
-    def create_project(self):
-        # Get the project name, license, and project directory from the text boxes
-        project_name = self.project_name_textbox.text()
-        license_option = self.license_dropdown.currentText()
-        github_url = self.github_url_textbox.text()
-        project_directory = self.project_directory_textbox.text()
+        main_layout = QHBoxLayout()
+        main_layout.addWidget(left_widget)
+        main_layout.addWidget(self.right_widget)
+        main_layout.setStretch(0, 40)
+        main_layout.setStretch(1, 200)
+        main_widget = QWidget()
+        main_widget.setLayout(main_layout)
+        self.setCentralWidget(main_widget)
+ 
+    def button1(self):
+        self.right_widget.setCurrentIndex(0)
 
-        # Create the project folder in the selected directory
-        os.makedirs(os.path.join(project_directory, project_name), exist_ok=True)
+    def button2(self):
+        self.right_widget.setCurrentIndex(1)
 
-        # Change into the new folder
-        os.chdir(os.path.join(project_directory, project_name))
+    def button3(self):
+        self.right_widget.setCurrentIndex(2)
+ 
+    def ui1(self):
+        main_layout = QVBoxLayout()
+        main_layout.addWidget(NewProjectWindow())
+        main_layout.addStretch(5)
+        main = QWidget()
+        main.setLayout(main_layout)
+        return main
 
-        # Initialize a Git repository
-        subprocess.run(["git", "init"], check=True)
+    def ui2(self):
+        main_layout = QVBoxLayout()
+        main_layout.addWidget(ReadmeGeneratorGUI())
+        main_layout.addStretch(5)
+        main = QWidget()
+        main.setLayout(main_layout)
+        return main
 
-        # Create a README.md file
-        with open("README.md", "w") as readme_file:
-            readme_file.write("# " + project_name + "\n\nThis is my project.")
-
-        # Create a .gitignore file
-        with open(".gitignore", "w") as gitignore_file:
-            gitignore_file.write("/.history \n/.idea \n/.vscode")
-
-        # Create the appropriate license file based on the selected license option
-        if license_option == "Apache License 2.0":
-            with open("LICENSE", "w") as license_file:
-                license_file.write(APACHE_LICENSE_TEXT)
-        elif license_option == "BSD 3-Clause License":
-            with open("LICENSE", "w") as license_file:
-                license_file.write(BSD_LICENSE_TEXT)
-        elif license_option == "MIT License":
-            with open("LICENSE", "w") as license_file:
-                license_file.write(MIT_LICENSE_TEXT)
-
-        # Add the files to Git
-        subprocess.run(["git", "add", "README.md", "LICENSE", ".gitignore"], check=True)
-
-        # Commit the files
-        subprocess.run(["git", "commit", "-m", "Initial commit"], check=True)
-        
-        if github_url:
-            # Push the changes to GitHub
-            subprocess.run(["git", "remote", "add", "origin", github_url], check=True)
-            subprocess.run(["git", "push", "-u", "origin", "master"], check=True)
-
-        # Show a message box to confirm that the project was created
-        confirmation_message = f"Project {project_name} created with {license_option} license."
-        self.show_message_box("Success", confirmation_message)
+    def ui3(self):
+        main_layout = QVBoxLayout()
+        about_widget = AboutWidget()
+        main_layout.addWidget(about_widget)
+        main_layout.addStretch(5)
+        main = QWidget()
+        main.setLayout(main_layout)
+        return main
 
 
-    def show_message_box(self, title, message):
-        from PyQt5.QtWidgets import QMessageBox
-
-        message_box = QMessageBox(self)
-        message_box.setWindowTitle(title)
-        message_box.setText(message)
-        message_box.addButton(QMessageBox.Ok)
-        message_box.exec_()
-        
-    def browse_directory(self):
-        # Open a file dialog to select the project directory
-        from PyQt5.QtWidgets import QFileDialog
-
-        options = QFileDialog.Options()
-        options |= QFileDialog.ShowDirsOnly
-        directory = QFileDialog.getExistingDirectory(self, "Select Directory", "", options=options)
-
-        # Set the selected directory in the text box
-        if directory:
-            self.project_directory_textbox.setText(directory)
-            
-    
-
-
-# License texts
-APACHE_LICENSE_TEXT = """\
-Apache License
-Version 2.0, January 2004
-http://www.apache.org/licenses/
-"""
-
-BSD_LICENSE_TEXT = """\
-BSD 3-Clause License
-For the full license text, please see the LICENSE file in
-"""
-
-MIT_LICENSE_TEXT = """\
-MIT License
-For the full license text, please see the LICENSE file in
-"""
-
-
-
-
+ 
 if __name__ == '__main__':
-    # Create the application and window instances
     app = QApplication(sys.argv)
-    window = NewProjectWindow()
-
-    # Show the window
-    window.show()
-
-    # Run the event loop
+    ex = Window()
+    ex.show()
     sys.exit(app.exec_())
