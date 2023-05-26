b7d41ce59fa36648c8d3f41738946e439dfa4476
Shrine Sabu <shrine02sabu@gmail.com>
2023-05-26 18:19:27 +0530

    added json format/validate

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from PyQt5.QtWidgets import *
+from PyQt5.QtWidgets import QApplication, QMainWindow, QListWidget, QWidget, QVBoxLayout, QStackedWidget, QHBoxLayout
 import sys
 from gui.new_project_window import NewProjectWindow
 from gui.readme_generator_gui import ReadmeGeneratorGUI
 from gui.about_widget import AboutWidget
+from gui.jsonformatvalidator import JSONFormatValidator
+
 
 
 class Window(QMainWindow):
@@ -15,62 +17,50 @@ class Window(QMainWindow):
         self.height = int(0.618 * self.Width)
         self.resize(self.Width, self.height)
 
-        self.btn_1 = QPushButton('New Project', self)
-        self.btn_2 = QPushButton('Readme', self)
-        self.btn_3 = QPushButton('About Us', self)
+        self.sidebar = QListWidget()
 
-        self.btn_1.clicked.connect(self.button1)
-        self.btn_2.clicked.connect(self.button2)
-        self.btn_3.clicked.connect(self.button3)
+        self.sidebar.addItem('New Project')
+        self.sidebar.addItem('JSON Format/Validate')
+        self.sidebar.addItem('Readme')
+        self.sidebar.addItem('About Us')
 
         self.tab1 = self.ui1()
         self.tab2 = self.ui2()
         self.tab3 = self.ui3()
+        self.tab4 = self.ui4()
 
         self.initUI()
 
     def initUI(self):
-        left_layout = QVBoxLayout()
-        left_layout.addWidget(self.btn_1)
-        left_layout.addWidget(self.btn_2)
-        left_layout.addWidget(self.btn_3)
-        left_layout.addStretch(5)
-        left_layout.setSpacing(20)
-        left_widget = QWidget()
-        left_widget.setLayout(left_layout)
+        sidebar_widget = QWidget()
+        sidebar_layout = QVBoxLayout()
+        sidebar_layout.addWidget(self.sidebar)
+        sidebar_widget.setLayout(sidebar_layout)
 
-        self.right_widget = QTabWidget()
-        self.right_widget.tabBar().setObjectName("mainTab")
+        # Increase the width of the sidebar
+        sidebar_widget.setFixedWidth(200)
 
-        self.right_widget.addTab(self.tab1, '')
-        self.right_widget.addTab(self.tab2, '')
-        self.right_widget.addTab(self.tab3, '')
-
-        self.right_widget.setCurrentIndex(0)
-        self.right_widget.setStyleSheet('''QTabBar::tab{width: 0; \
-            height: 0; margin: 0; padding: 0; border: none;}''')
+        stacked_widget = QStackedWidget()
+        stacked_widget.addWidget(self.tab1)
+        stacked_widget.addWidget(self.tab2)
+        stacked_widget.addWidget(self.tab3)
+        stacked_widget.addWidget(self.tab4)
 
         main_layout = QHBoxLayout()
-        main_layout.addWidget(left_widget)
-        main_layout.addWidget(self.right_widget)
-        main_layout.setStretch(0, 40)
-        main_layout.setStretch(1, 200)
+        main_layout.addWidget(sidebar_widget)
+        main_layout.addWidget(stacked_widget)
+        main_layout.setStretch(0, 0)  # Disable stretching for the sidebar
+
         main_widget = QWidget()
         main_widget.setLayout(main_layout)
         self.setCentralWidget(main_widget)
 
-    def button1(self):
-        self.right_widget.setCurrentIndex(0)
-
-    def button2(self):
-        self.right_widget.setCurrentIndex(1)
-
-    def button3(self):
-        self.right_widget.setCurrentIndex(2)
+        self.sidebar.currentRowChanged.connect(stacked_widget.setCurrentIndex)
 
     def ui1(self):
         main_layout = QVBoxLayout()
-        main_layout.addWidget(NewProjectWindow())
+        new_project_window = NewProjectWindow()
+        main_layout.addWidget(new_project_window)
         main_layout.addStretch(5)
         main = QWidget()
         main.setLayout(main_layout)
@@ -93,9 +83,19 @@ class Window(QMainWindow):
         main.setLayout(main_layout)
         return main
 
+    def ui4(self):
+        main_layout = QVBoxLayout()
+        main_layout.addWidget(JSONFormatValidator())
+        main_layout.addStretch(5)
+        main = QWidget()
+        main.setLayout(main_layout)
+        return main
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+    app.setStyle('macintosh')
+
     ex = Window()
     ex.show()
     sys.exit(app.exec_())
